1d371cdb5c914ef0b249259e6b269077b5781af8
Ariav Hayempour <ariavhayempour@gmail.com>
2025-12-19 18:30:51 -0800

    Cinn city schedule special handling

--- client/src/pages/cart-detail.tsx
+++ client/src/pages/cart-detail.tsx
@@ -168,6 +168,20 @@ export default function IndividualFoodCartDetailPage() {
                       </div>
                     </CardContent>
                   </Card>
+                ) : cart.slug === "cinn-city" ? (
+                  <Card>
+                    <CardHeader>
+                      <CardTitle className="flex items-center">
+                        <Clock className="w-5 h-5 mr-2" />
+                        Hours
+                      </CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                      <p className="text-gray-600">
+                        Currently in their off-season. Schedule will be updated when they are running!
+                      </p>
+                    </CardContent>
+                  </Card>
                 ) : cart.slug === "stellies" ? (
                   <Card>
                     <CardHeader>
