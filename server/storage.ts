ca9280d4ab895bb9d9aeff43ed643c47b6cde49a
Ariav Hayempour <ariavhayempour@gmail.com>
2025-12-24 21:24:57 -0800

    Version control test:  intentional error in commit

--- server/storage.ts
+++ server/storage.ts
@@ -15,7 +15,7 @@ export class MemStorage implements IStorage {
   constructor() {
     this.carts = new Map();
     this.currentId = 1;
-    this.seedData();
+    this.seedfffeData();
   }
 
   private seedData() {
